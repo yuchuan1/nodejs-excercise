60ee0b546b677bf57e974ab67ede7e8369b00c20
Eddie Chen <yuchuan1@gmail.com>
2014-12-16 10:50:19 +0800

    add parameter passing to c++ addon

--- NODE_ADDON/index.js
+++ NODE_ADDON/index.js
@@ -3,6 +3,6 @@ var path = require('path')
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 var binding = require(binding_path);
 
-require('assert').equal(binding.hello(),"hello from c++");
+require('assert').equal(binding.hello("hello from c++"),"hello from c++");
 
-console.log(binding.hello());
+console.log(binding.hello("Hello from JavaScript and C++"));
--- NODE_ADDON/node_addon.cc
+++ NODE_ADDON/node_addon.cc
@@ -12,7 +12,10 @@
     static void get_hello(const v8::FunctionCallbackInfo<v8::Value>& args)
     {
         v8::HandleScope scope(v8::Isolate::GetCurrent());
-        MyLib::Message msg("hello from c++");
+        v8::String::Utf8Value  param1(args[0]->ToString());
+        std::string mystr = std::string(*param1);
+        MyLib::Message msg(mystr);
+        //MyLib::Message msg("hello from c++");
         std::string msg_string = msg.get();
         args.GetReturnValue().Set(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(),msg_string.c_str()));
     }
@@ -22,7 +25,10 @@
     static v8::Handle<v8::Value> get_hello(const v8::Arguments& args)
     {
         v8::HandleScope scope;
-        MyLib::Message msg("hello from c++");
+        v8::String::Utf8Value  param1(args[0]->ToString());
+        std::string mystr = std::string(*param1);
+        MyLib::Message msg(mystr);
+       // MyLib::Message msg("hello from c++");
         std::string msg_string = msg.get();
         return scope.Close(v8::String::New(msg_string.c_str()));
     }
